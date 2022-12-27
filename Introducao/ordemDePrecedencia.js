ca4ccc856a0c73d5db75f51116e2fcdf600200cc
Joao Victor <victorjv3288@gmail.com>
2022-12-26 21:34:32 -0300

    Create ordemDePrecedencia.js

---
+++ Introducao/ordemDePrecedencia.js
@@ -0,0 +1,73 @@
+// ordem de precedencia
+/*
+    1º ()
+    2º **
+    3º * / %
+    4º + -              
+*/
+
+// operadores de atribuição
+/*
+    = atribuição
+    += atribuição de adição
+    -= atribuição de subtração
+    *= atribuição de multiplicação
+    /= atribuição de divisão
+    %= atribuição de resto
+    **= atribuição de exponenciação
+*/
+
+// operadores de comparação
+/*
+    == igualdade (valor)
+    === igualdade estrita (valor e tipo)
+    != diferente (valor)
+    !== diferente estrito (valor e tipo)
+    > maior que
+    >= maior ou igual que
+    < menor que
+    <= menor ou igual que
+*/
+
+// operadores var
+/*
+    var a = 5 + 3
+    var b = a % 5
+    var c = 5 * b ** 2
+    var d = 10 - a / 2
+    var e = 6 * 2 / d
+    var f = b % e + 4 / e
+*/
+
+var a = 5 + 3
+var b = a % 5
+var c = 5 * b ** 2
+var d = 10 - a / 2
+var e = 6 * 2 / d
+var f = b % e + 4 / e
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+
+// operadores logicos
+/*
+    && e
+    || ou
+    ! não
+*/
+
+// operadores de incremento e decremento
+/*
+    ++ incremento
+    -- decremento
+*/
+
+// operadores ternarios
+/*
+    condicao ? valor1 : valor2
+*/
+
